6d62fe79bd9d584e3c396b78137024e6e58a1918
Paris Lyons <parislyons@hotmail.co.uk>
2022-03-10 14:13:13 +0000

    PL: unit tests getting full coverage and pass rate

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,23 +5,26 @@ from application import app, db
 from application.models import Albums, Songs, Listings
 
 class TestBase(TestCase):
-    def createapp(self):
+    def create_app(self):
         app.config.update(
-            SQLALCHEMY_DATABASE_URI='SQLITE:///data.db',
+            SQLALCHEMY_DATABASE_URI='sqlite:///',
             SECRET_KEY='TEST_SECRET_KEY',
             DEBUG=True,
             WTF_CSRF_ENABLED=False
         )
         return app
 
-    def setup(self):
+    def setUp(self):
         db.create_all()
-        db.session.add(Albums)
-        db.session.add(Songs)
-        db.session.add(Listings)
+        album = Albums(album_name="Lipgloss",  release_year=2000)
+        db.session.add(album)
+        song = Songs(song_name="Cherry", song_length="2:30")
+        db.session.add(song)
+        listing = Listings(album_id=1,  song_id=1)
+        db.session.add(listing)
         db.session.commit()
 
-    def teardown(self):
+    def tearDown(self):
         db.session.remove()
         db.drop_all()
 
@@ -47,14 +50,14 @@ class TestViews(TestBase):
         self.assert200(response)
 
     def test_edit_song_get(self):
-        response = self.client.get(url_for('editsong'))
+        response = self.client.get(url_for('edit'))
         self.assert200(response)
 
 class TestRead(TestBase):
 
     def test_read_home_tasks(self):
         response = self.client.get(url_for('index'))
-        self.assertIn(b"Run unit tests", response.data)
+        self.assertIn(b"Lipgloss", response.data)
 
 
 class TestAdd(TestBase):
@@ -62,48 +65,49 @@ class TestAdd(TestBase):
     def test_add_album_task(self):
         response = self.client.post(
             url_for('addalbum'),
-            data={"album_name": "Test"},
-            data={"release_date": "2000"},
+            data={"album_name": "Fresh", "release_year": 2000},
             follow_redirects=True
         )
-        self.assertIn(b"Testing create functionality", response.data)
+        new_album = Albums.query.get(2)
+        self.assertEquals(new_album.album_name, "Fresh")
 
     def test_add_song_task(self):
         response = self.client.post(
             url_for('addsong'),
-            data={"song_name": "Test Song"},
-            data={"song_length": "2:59"},
+            data={"song_name": "Test Song", "song_length": "2:59"},
             follow_redirects=True
         )
-        self.assertIn(b"Testing create functionality", response.data)
+        new_song = Songs.query.get(2)
+        self.assertEquals(new_song.song_name, "Test Song")
+        self.assertIn(b"Test Song", response.data)
     
     def test_add_song_to_album_task(self):
+        song = Songs(song_name="Lime", song_length="4:30")
+        db.session.add(song)
+        db.session.commit()
         response = self.client.post(
             url_for('addsongtoalbum'),
-            data={"song_id": "1"},
-            data={"album_id": "1"},
+            data={"song_id": 2, "album_id": 1},
             follow_redirects=True
         )
-        self.assertIn(b"Testing create functionality", response.data)
+        self.assertIn(b"Lime", response.data)
     
 class TestUpdate(TestBase):
 
     def test_edit_song_task(self):
         response = self.client.post(
-            url_for('editsong'),
-            data={"song_id": "1"},
-            data={"new_song_name": "Edit Song"},
-            data={"new_song_length": "3:01"},
+            url_for('edit'),
+            data={"song_id": "1", "new_song_name": "Edit Song", "new_song_length": "3:01"},
             follow_redirects=True
         )
-        self.assertIn(b"Testing update functionality", response.data)
+        self.assertIn(b"Edit Song", response.data)
 
 class TestDelete(TestBase):
 
     def test_delete_album_task(self):
-        response = self.client.get(
+        response = self.client.post(
             url_for('deletealbum'),
-            data={"album_id": "1"},
+            data={"album_id": 1},
             follow_redirects=True
         )
-        self.assertNotIn(b"Run unit tests", response.data)
+        self.assertNotIn(b"Lipgloss", response.data)
