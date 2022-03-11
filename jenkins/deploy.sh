cd0963468569e8770e242225901d3f469c3a8a2f
parislyons <parislyons@hotmail.co.uk>
2022-03-11 14:20:51 +0000

    PL: mysql credential fix

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,5 +4,5 @@ echo "Deploy stage"
 scp docker-compose.yaml jenkins@BFPDocker:/home/jenkins/docker-compose.yaml
 ssh jenkins@BFPDocker \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
-    MY_SQL_ROOT_PASSWORD=$MY_SQL_ROOT_PASSWORD \
+    MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
     docker stack deploy --compose-file docker-compose.yaml band-flask-pro
