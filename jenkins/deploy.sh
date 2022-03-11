7bc7833b5b923cf1d8c7ff5d39663579dd088624
parislyons <parislyons@hotmail.co.uk>
2022-03-11 12:18:40 +0000

    PL: updated deploy.sh for mysql container fix

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,5 +4,5 @@ echo "Deploy stage"
 scp docker-compose.yaml jenkins@BFPDocker:/home/jenkins/docker-compose.yaml
 ssh jenkins@BFPDocker \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
-    DATABASE_URI=$DATABASE_URI \
-    docker stack deploy --compose-file docker-compose.yaml Band-Flask-Pro
+    MY_SQL_ROOT_PASSWORD=$MY_SQL_ROOT_PASSWORD \
+    docker stack deploy --compose-file docker-compose.yaml band-flask-pro
